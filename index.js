517c441c7da921947c03b8b972fd82eece892eec
Juliannn <161928537+wucebrayn3@users.noreply.github.com>
2025-02-15 21:24:26 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const bg1 = document.getElementById("bg1");
 const bg2 = document.getElementById("bg2");
 const bg3 = document.getElementById("bg3");
 let counter = 1;
+let rotateAngle = "360deg"
+let rotateAngle2 = "-360deg"
+
 
 bg3.addEventListener("click", ()=>{
     if(counter === 3){
         counter = 0
     };
+    rotateAngle = rotateAngle === "360deg" ? "-360deg":"360deg"
+    rotateAngle2 = rotateAngle2 === "-360deg" ? "360deg":"-360deg"
+    console.log(rotateAngle)
     counter++;
     bg3.style.backgroundImage = `url('images/bg_${counter}.jpg')`
-    bg3.style.rotate = "360deg"
+    bg3.style.rotate = `${rotateAngle}`
     bg2.style.backgroundImage = `url('images/bg_${counter}.jpg')`
-    bg2.style.rotate = "-360deg"
+    bg2.style.rotate = `${rotateAngle2}`
     bg1.style.backgroundImage = `url('images/bg_${counter}.jpg')`
 })
